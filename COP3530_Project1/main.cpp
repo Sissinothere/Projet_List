95c85517060a8e65b995d1578260a7e1b01f5b67
Xixi <flyskyxi@gmail.com>
2014-10-02 09:41:31 -0400

    Initial Commit

---
+++ COP3530_Project1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  COP3530_Project1
+//
+//  Created by Sissi on 10/2/14.
+//  Copyright (c) 2014 Sissi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
